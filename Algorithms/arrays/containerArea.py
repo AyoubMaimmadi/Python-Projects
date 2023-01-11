2dbdff1785458e4dc21b554adba5c71fdce005d1
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2023-01-12 00:31:06 +0100

    new

---
+++ Algorithms/arrays/containerArea.py
@@ -0,0 +1,17 @@
+nums = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+
+def containerArea(list): 
+    res = 0
+    l, r = 0, len(list)-1
+
+    while l < r:
+        area = (r - l) * min(list[r], list[l])
+        res = max(res, area)
+        if list[r] > list[l]:
+            l += 1
+        else: 
+            r -= 1
+    return res
+        
+print(containerArea(nums))
+        
