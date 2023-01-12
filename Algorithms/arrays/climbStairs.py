da51dae327c6b5b2f07fb45a97ff1f16211a508a
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2023-01-12 12:17:04 +0100

    new

---
+++ Algorithms/arrays/climbStairs.py
@@ -0,0 +1,12 @@
+def climbNumOfStairs(n):
+
+    one, two = 1, 1
+
+    for i in range(n - 1):
+        temp = one
+        one = one + two
+        two = temp
+    
+    return one
+
+print(climbNumOfStairs(2))
