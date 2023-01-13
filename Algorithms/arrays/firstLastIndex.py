08a404830d594abb3c63f40538ced6dfb1337618
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2023-01-13 13:41:03 +0100

    new

---
+++ Algorithms/arrays/firstLastIndex.py
@@ -0,0 +1,57 @@
+def firstLastIndex(list, target):
+
+    for i in range(len(list)):
+        if list[i] == target:
+            start = i
+            while i + 1 < len(list) and list[i + 1] == target:
+                i += 1
+            return [start, i]
+    return [-1, -1]
+
+
+def findStart(arr, target):
+    if arr[0] == target:
+        return 0
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target and arr[mid - 1] < target:
+            return mid
+        elif arr[mid] < target: #[1, 3, 3, 3, 3, 3, 3, 5, 7, 8, 9]
+            left = mid + 1
+        else: 
+            right = mid - 1
+    return -1
+
+def findEnd(arr, target):
+    if arr[-1] == target:
+        return (len(arr) - 1)
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target and arr[mid + 1] > target:
+            return mid
+        elif arr[mid] > target: #[1, 3, 3, 3, 3, 3, 3, 5, 7, 8, 9]
+            right = mid - 1
+        else: 
+            left = mid + 1
+    return -1
+
+
+def firstLastIndexBinSearch(arr, target):
+    if len(arr) == 0:
+        return [-1, -1]
+    else: 
+        start = findStart(arr, target)
+        end = findEnd(arr, target)
+        return [start, end]
+    return [-1, -1]
+
+
+
+A = [1, 3, 3, 3, 3, 3, 3, 5, 7, 8, 9]
+
+
+print("firstLastIndex: " , firstLastIndex(A, 3))
+print("firstLastIndexBinSearch: " , firstLastIndexBinSearch(A, 3))
+
