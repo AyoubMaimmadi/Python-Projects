c93f17f1e9f055b9100f6c80d5abc16d680828d1
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2023-01-14 22:12:30 +0100

    new

---
+++ A-preparation/removeLinkListElem.py
@@ -0,0 +1,21 @@
+class ListNode:
+    def init_(self, val=0, next=None) :
+        self. val = val
+        self.next = next
+
+class Solution :
+    def removeElements (self, head: ListNode, val: int) -> ListNode :
+        dummy = ListNode(next=head)
+        prev, curr = dummy, head
+        while curr:
+            nxt = curr . nxt
+            if curr.val == val:
+                prev.next = nxt 
+            else:
+                prev = Curr
+                
+            Curr= nxt
+        return dummy . next
+
+
+        
