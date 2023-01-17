1983768a430ca7389ebe42e4c4f4fdf1ae5aab44
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2023-01-17 13:12:59 +0100

    new

--- A-preparation/searchInsert.py
+++ A-preparation/searchInsert.py
@@ -13,7 +13,9 @@ class Solution:
                 return mid
 
             if target > nums[mid] :
-                l = mid +1
+                l = mid + 1
 
             else:
                 r = mid - 1
+
+        return l
