467f58750c80815e176e6710a81a213bff781774
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2023-01-17 12:41:51 +0100

    new

---
+++ A-preparation/dfs_bfs.py
@@ -0,0 +1,19 @@
+def dfs(graph, start):
+    visited = set()
+    stack = [start]
+    while stack:
+        vertex = stack.pop()
+        if vertex not in visited:
+            visited.add(vertex)
+            stack.extend(graph[vertex] - visited)
+    return visited
+
+def bfs(graph, start):
+    visited = set()
+    queue = [start]
+    while queue:
+        vertex = queue.pop(0)
+        if vertex not in visited:
+            visited.add(vertex)
+            queue.extend(graph[vertex] - visited)
+    return visited
